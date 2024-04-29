fd50616dd21cb7ff31efb0a758c20521da2c818b
dawsol2038 <142831883+dawsol2038@users.noreply.github.com>
2024-04-29 13:30:02 -0400

    Update App.java
    Added "Add New Batter" Menu

--- CSD2522BatterTeamFinalProjectGUI/src/main/java/com/mycompany/mavenproject1/App.java
+++ CSD2522BatterTeamFinalProjectGUI/src/main/java/com/mycompany/mavenproject1/App.java
@@ -11,6 +11,7 @@ Luke Dawson - 4/26/24 - added integer parsing for the text fields before adding
     data to the database
 Luke Dawson - 4/28/24 - added formatting to clean up the GUI and make it more manageable
 Terry Pescosolido - 4/28/24 - changed calls to database for integration
+Luke Dawson - 4/29/24 - added "Add new Batter" menu
 */
 
 package com.mycompany.mavenproject1;
@@ -42,7 +43,7 @@ import java.util.List;
 public class App extends Application {
     // create labels
     private Label gameNumberLabel = new Label("Game #");
-    private Label playerNumberLabel = new Label("Player #");
+    private Label playerLabel = new Label("Player:");
     private Label starterLabel = new Label("Starter");
     private Label battingOrderLabel = new Label("Batting Order #");
     private Label atBatLabel = new Label("At-Bats:");
@@ -60,10 +61,14 @@ public class App extends Application {
     private Label stolenBaseSuccessLabel = new Label("Stolen Base Successes:");
     private Label sacrificeFliesLabel = new Label("Sacrifice Flies:");
     private Label leftOnBaseLabel = new Label("Left on Base:");
+    private Label firstNameLabel = new Label("First Name:");
+    private Label lastNameLabel = new Label("Last Name:");
+    private Label enterPlayerNumberLabel = new Label("Player Number:");
+    private Label activeLabel = new Label("Active");
     
     // create textfields
     private TextField gameNumberField = new TextField("0");
-    private TextField playerNumberField = new TextField("0");
+    ComboBox<String> playerComboBox = new ComboBox<>();
     private CheckBox starterCheckBox = new CheckBox("");
     private TextField battingOrderField = new TextField("0");
     private TextField atBatField = new TextField("0");
@@ -81,10 +86,14 @@ public class App extends Application {
     private TextField stolenBaseSuccessField = new TextField("0");
     private TextField sacrificeFliesField = new TextField("0");
     private TextField leftOnBaseField = new TextField("0");
+    private TextField firstNameField = new TextField();
+    private TextField lastNameField = new TextField();
+    private TextField enterPlayerNumberField = new TextField();
+    private CheckBox activeCheckBox = new CheckBox("");
     
     BaseballStatsDB baseball_stats_db = new BaseballStatsDB();
     List<Batter> batters = null; 
-    List<Player> players = null; 
+    private List<Player> players = new ArrayList<>();
     
     // create the primary stage for the main menu
     private Stage primaryStage;
@@ -103,21 +112,24 @@ public class App extends Application {
         grid.setVgap(10);
     
         // create the scene
-        Scene scene = new Scene(grid, 250, 190);
+        Scene scene = new Scene(grid, 250, 230);
 
         // create buttons for different actions
         Button enterDataButton = new Button("Enter Batter Stats");
+        Button enterPlayerButton = new Button("Enter New Batter");
         Button viewGameReportButton = new Button("View Game Report");
         Button viewMultiGameReportButton = new Button("View Multi-Game Report");
         
         // VBox to hold the different options the user can choose from
         VBox optionBox = new VBox(10);
         optionBox.getChildren().add(enterDataButton);
+        optionBox.getChildren().add(enterPlayerButton);
         optionBox.getChildren().add(viewGameReportButton);
         optionBox.getChildren().add(viewMultiGameReportButton); 
         grid.add(optionBox, 0, 0, 3, 1);
         
         enterDataButton.setOnAction(event -> enterDataButtonClicked());
+        enterPlayerButton.setOnAction(event -> enterBatterButtonClicked());
         
         // exit button
         Button exitButton = new Button("Exit");
@@ -149,9 +161,14 @@ public class App extends Application {
         enterDataGrid.setHgap(10);
         enterDataGrid.setVgap(10);
         
+        for (Player player : players) {
+            playerComboBox.getItems().add(player.getPlayerName()); // Add player names to ComboBox
+        }
+        enterDataGrid.add(playerComboBox, 1, 1, 1, 1);
+        
         VBox labelBox1 = new VBox(18);
         labelBox1.getChildren().add(gameNumberLabel);
-        labelBox1.getChildren().add(playerNumberLabel);
+        labelBox1.getChildren().add(playerLabel);
         labelBox1.getChildren().add(starterLabel);
         labelBox1.getChildren().add(battingOrderLabel);
         labelBox1.getChildren().add(atBatLabel);
@@ -176,7 +193,7 @@ public class App extends Application {
         
         VBox textFieldBox1 = new VBox(10);
         textFieldBox1.getChildren().add(gameNumberField);
-        textFieldBox1.getChildren().add(playerNumberField);
+        textFieldBox1.getChildren().add(playerComboBox);
         textFieldBox1.getChildren().add(starterCheckBox);
         textFieldBox1.getChildren().add(battingOrderField);
         textFieldBox1.getChildren().add(atBatField);
@@ -216,7 +233,50 @@ public class App extends Application {
         submitReturnBox.getChildren().add(returnButton);
         enterDataGrid.add(submitReturnBox, 0, 1, 2, 1);
 
-        // aet the new scene
+        // set the new scene
+        Scene enterDataScene = new Scene(enterDataGrid);
+        primaryStage.setScene(enterDataScene);
+    }
+    
+    private void enterBatterButtonClicked() {
+        // create a new scene for entering data
+        GridPane enterDataGrid = new GridPane();
+        enterDataGrid.setAlignment(Pos.TOP_LEFT);
+        enterDataGrid.setPadding(new Insets(25, 25, 25, 25));
+        enterDataGrid.setHgap(10);
+        enterDataGrid.setVgap(10);
+        
+        VBox labelBox = new VBox(20);
+        labelBox.getChildren().add(firstNameLabel);
+        labelBox.getChildren().add(lastNameLabel);
+        labelBox.getChildren().add(enterPlayerNumberLabel);
+        labelBox.getChildren().add(activeLabel);
+        enterDataGrid.add(labelBox, 0, 0);
+        
+        VBox textBox = new VBox(10);
+        textBox.getChildren().add(firstNameField);
+        textBox.getChildren().add(lastNameField);
+        textBox.getChildren().add(enterPlayerNumberField);
+        textBox.getChildren().add(activeCheckBox);
+        enterDataGrid.add(textBox, 1, 0);
+        
+        Button submitButton = new Button("Submit");
+        submitButton.setOnAction(event -> submitPlayerButtonClicked());
+        // add a reset button
+        Button resetButton = new Button("Reset");
+        resetButton.setOnAction(event -> resetButtonClicked());
+        // add a return button
+        Button returnButton = new Button("Return");
+        returnButton.setOnAction(event -> returnButtonClicked());
+        
+        // HBox to hold the submit and return buttons
+        HBox submitReturnBox = new HBox(10);
+        submitReturnBox.getChildren().add(submitButton);
+        submitReturnBox.getChildren().add(resetButton);
+        submitReturnBox.getChildren().add(returnButton);
+        enterDataGrid.add(submitReturnBox, 0, 1, 2, 1);
+        
+        // set the new scene
         Scene enterDataScene = new Scene(enterDataGrid);
         primaryStage.setScene(enterDataScene);
     }
@@ -226,7 +286,7 @@ public class App extends Application {
 //        try {
             // Retrieve the data entered in each field
             int gameNumber = Integer.parseInt(gameNumberField.getText());
-            int batter_pn = Integer.parseInt(playerNumberField.getText());
+            // add code to get values from the combo box
             int batter_gs = starterCheckBox.isSelected() ? 1 : 0; // convert boolean to int
             int batter_bo = Integer.parseInt(battingOrderField.getText());
             int batter_ab = Integer.parseInt(atBatField.getText());
@@ -275,6 +335,32 @@ public class App extends Application {
 //        }
     }
     
+    // method to handle the submit button on the enter batter menu
+    private void submitPlayerButtonClicked() {
+        // Retrieve the data entered in each field
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        int playerNumber = Integer.parseInt(enterPlayerNumberField.getText());
+        boolean isActive = activeCheckBox.isSelected();
+
+        // Create a new Player object with the entered data
+        Player player = new Player(playerNumber, firstName + " " + lastName, isActive);
+
+        // Add the new player to the list
+        players.add(player);
+
+        // Show success message
+        String activeStatus = isActive ? "active" : "inactive";
+        Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+        successAlert.setTitle("Success");
+        successAlert.setHeaderText("Player added successfully");
+        successAlert.setContentText("Player " + firstName + " " + lastName + " added with player number " + playerNumber + " and is " + activeStatus);
+        successAlert.showAndWait();
+
+        // Clear fields
+        resetButtonClicked();
+    }
+    
     // function to return back to the main menu
     private void returnButtonClicked() {
         // navigate back to the main menu
@@ -285,7 +371,7 @@ public class App extends Application {
     private void resetButtonClicked() {
         // reset all data input
         gameNumberField.setText("0");
-        playerNumberField.setText("0");
+        playerComboBox.getSelectionModel().clearSelection();
         starterCheckBox.setSelected(false);
         battingOrderField.setText("0");
         atBatField.setText("0");
@@ -303,6 +389,12 @@ public class App extends Application {
         stolenBaseSuccessField.setText("0");
         sacrificeFliesField.setText("0");
         leftOnBaseField.setText("0");
+        
+        // reset data input for enter player menu
+        firstNameField.setText("");
+        lastNameField.setText("");
+        enterPlayerNumberField.setText("");
+        activeCheckBox.setSelected(false);
     }
     
     // function to end the program
