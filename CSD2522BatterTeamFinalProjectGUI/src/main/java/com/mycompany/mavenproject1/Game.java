b0a786f84973cc6239db3477f80c2f88f8f676e5
pescokid1 <51890846+pescokid1@users.noreply.github.com>
2024-04-30 15:09:32 -0400

    added Game.java to add all interaction with game data

---
+++ CSD2522BatterTeamFinalProjectGUI/src/main/java/com/mycompany/mavenproject1/Game.java
@@ -0,0 +1,53 @@
+/*
+    Created by:  xxx
+    Created on:  05/10/2024
+    Purpose:     Game method
+    Description: This method stores all the info related to games.
+
+Terry Pescosolido - 4/30/24 - created
+*/
+
+package com.mycompany.mavenproject1;
+
+public class Game {
+
+    private int game_number;
+    private String game_opponent_name;
+    private String game_date;
+    
+    public Game() {
+        this(0, "", "");
+    }
+
+    public Game(int game_number, String game_opponent_name, String game_date) { 
+        this.game_number = game_number;
+        this.game_opponent_name = game_opponent_name;
+        this.game_date = game_date;
+    }
+    
+    public void setGameNumber(int game_number) {
+        this.game_number = game_number;
+    }
+    
+    public int getGameNumber() {
+        return game_number;
+    }
+
+    public void setGameOpponentName(String game_opponent_name) {
+        this.game_opponent_name = game_opponent_name;
+    }
+    
+    public String getGameOpponentName() {
+        return game_opponent_name;
+    }
+ 
+    public void setGameActive(String game_date) {
+        this.game_date = game_date;
+    }
+        
+    public String getGameDate() {
+        return game_date;
+    }
+   
+}
+
